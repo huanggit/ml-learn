cce0b4ebfd7b586d80fc9a822b7d73db268e3f9b
huanggy <huanggy121@163.com>
2018-09-02 09:20:49 +0800

    add plot

--- main.py
+++ main.py
@@ -7,34 +7,39 @@ from optimizer import SGD, Momentum
 from loss import loss_mse
 
 
-def plot_data_and_model(data, model):
+def init_plot():
     # plot.rcParams['font.sans-serif'] = 'SimHei'
     # plot.rcParams['axes.unicode_minus'] = False
-    plot.xlabel('x')
-    plot.ylabel('y')
-    # plot data
-    plot.scatter(data.x, data.y)
-    # plot model
-    x_, y_ = model_sampling(model)
-    plot.plot(x_, y_, color='green')
-    plot.show()
+    fig = plot.figure(1, figsize=(14, 7))
+    fig.suptitle('Optimizers', fontsize=15)
 
 
-def test_gradient_descent():
-    model = LinearModel() \
-        .compile(loss_func=loss_mse, optimizer=SGD()) \
-        .fit(linear_dataset(), epoch=50)
+def plot_model(data, model, inx):
+    def plot_data_and_model():
+        plot.subplot(2, 4, inx + 1)
+        plot.scatter(data.x, data.y)
+        x_, y_ = model_sampling(model)
+        plot.plot(x_, y_, color='green')
+        plot.title(model.optimizer.__class__.__name__)
 
-
-def test_sgd():
-    model = LinearModel() \
-        .compile(loss_func=loss_mse, optimizer=SGD()) \
-        .fit(linear_dataset(), epoch=50, batch_size=4)
-    plot_data_and_model(linear_dataset(), model)
+    def plot_losses():
+        plot.subplot(2, 4, inx + 5)
+        plot.plot(range(50), model.losses, color='orange')
+    plot_data_and_model()
+    plot_losses()
 
 
 if __name__ == '__main__':
-    model = LinearModel() \
-        .compile(loss_func=loss_mse, optimizer=Momentum()) \
-        .fit(linear_dataset(), epoch=50)
-    plot_data_and_model(linear_dataset(), model)
+    init_plot()
+    dataset = linear_dataset()
+    for inx, optimizer in enumerate([SGD(), Momentum()]):
+        model = LinearModel() \
+            .compile(loss_func=loss_mse, optimizer=optimizer) \
+            .fit(dataset, epoch=50)
+        plot_model(dataset, model, inx)
+    plot.show()
+
+
+'''
+
+'''
